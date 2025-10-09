c6892e1baa3c58ca9efb3e25633317c7d06f0677
Maxi Soriano <maxi.soriano.70.23@gmail.com>
2025-10-09 17:54:03 -0300

    Se agrego cambios en el entidad registration

--- domain/src/entities/registration.ts
+++ domain/src/entities/registration.ts
@@ -1,10 +1,13 @@
-import { RegistrationStatus } from "../utils/enums/registrationStatus.js";
-import type { Entity } from "../utils/types/entity.js";
+import { RegistrationStatus } from "../utils/enums/registrationStatus";
+import { Entity } from "../utils/types/entity";
 
-export interface Registration extends Entity{
+export interface Registration extends Entity {
     studentId: string;
     courseId: string;
     enrollmentDate: Date;
     status: RegistrationStatus;
     completionDate?: Date;
+    totalFees?: number;
+    paidFees?: number;
+    certificateUrl?: string;
 }
