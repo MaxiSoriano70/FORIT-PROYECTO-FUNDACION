2f682bbad75e2d7814ae5ca356004f4b57b7a631
Maxi Soriano <maxi.soriano.70.23@gmail.com>
2025-10-12 11:20:25 -0300

    Se modifico el user-service

--- domain/src/services/user-service.ts
+++ domain/src/services/user-service.ts
@@ -5,4 +5,5 @@ export interface UserService extends Service<User>{
     findByFirstName: (firstName: string) => Promise<User | undefined>;
     findByLastName: (lastName: string) => Promise<User | undefined>;
     findByEmail: ( email: string ) => Promise<User | undefined>;
+    findByFullName: (firstName: string, lastName: string) => Promise<User | undefined>;
 }
