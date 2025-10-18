4d9314d42d9a04eefa3c18f0442439b0c8097299
Maxi Soriano <maxi.soriano.70.23@gmail.com>
2025-10-18 03:42:46 -0300

    Eliminacion CourseCategory

--- domain/src/utils/enums/courseCategory.ts
+++
@@ -1,8 +0,0 @@
-export const CourseCategory = {
-    PROGRAMACION: "Programación",
-    DISEÑO: "Diseño",
-    MARKETING: "Marketing",
-    IDIOMAS: "Idiomas"
-} as const;
-
-export type CourseCategory = (typeof CourseCategory)[keyof typeof CourseCategory];
