68819aeb5da453a390dfd36915133cdf96925777
Maxi Soriano <maxi.soriano.70.23@gmail.com>
2025-11-06 05:32:23 -0300

    Se creo Routes

---
+++ apps/frontend/src/shared/routes.ts
@@ -0,0 +1,11 @@
+export enum RoutePaths{
+    HOME = "",
+    TIPS = "tips",
+    USUARIOS = "usuarios",
+    CURSOS = "cursos",
+    ESTUDIANTES = "estudiantes",
+    INSCRIPCIONES = "inscripciones",
+    ESTUDIANTEDETALLE = "alumnodetalle",
+    USUARIODETALLE = "usuariodetalle",
+    ERROR404 = "**"
+}
