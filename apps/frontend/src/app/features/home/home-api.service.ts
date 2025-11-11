d98602b0712b4da8e9868716fe79898b91801522
Maxi Soriano <maxi.soriano.70.23@gmail.com>
2025-11-11 08:46:09 -0300

    Se agrego el service de home

---
+++ apps/frontend/src/app/features/home/home-api.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Course } from '../../../shared/entities/course';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HomeApiService {
+
+  baseUrl = "http://localhost:8080/api/";
+
+  constructor(private http: HttpClient) {}
+
+  getCursos(): Observable<Course[]> {
+    return this.http.get<{ message: string, data: Course[] }>(`${this.baseUrl}course`)
+      .pipe(
+        map(response => response.data)
+      );
+  }
+}
