19bc881e244954d9669f2e2be34d13c5c2ba33eb
Maxi Soriano <maxi.soriano.70.23@gmail.com>
2025-11-14 23:27:27 -0300

    Se agrego la page error404

---
+++ apps/frontend/src/app/features/error404/error404.component.ts
@@ -0,0 +1,17 @@
+import { Location } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-error404',
+  imports: [],
+  templateUrl: './error404.component.html',
+  styleUrl: './error404.component.css'
+})
+
+export class Error404Component {
+  constructor(private location: Location) {}
+
+  goBack(): void {
+    this.location.back();
+  }
+}
