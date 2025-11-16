69519d3f5e10389309aeba17d053e0dbc20eaedb
Maxi Soriano <maxi.soriano.70.23@gmail.com>
2025-11-15 23:50:44 -0300

    Se agrego el button de modal de informacion

--- apps/frontend/src/app/features/home/home.component.ts
+++ apps/frontend/src/app/features/home/home.component.ts
@@ -5,9 +5,14 @@ import { LoadingComponent } from "../../loading/loading.component";
 import { Observable } from 'rxjs';
 import { Course } from '../../../shared/entities/course';
 import { HomeApiService } from './home-api.service';
-import { EdteamComponent } from "../../edteam/edteam.component";
 import { RollingCodeComponent } from "../../rolling-code/rolling-code.component";
 import { FooterComponent } from "../../footer/footer.component";
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ModalAddFormInformationComponent } from '../../modal-add-form-information/modal-add-form-information.component';
+
+import { Store } from '@ngrx/store';
+import { User } from '../../../shared/entities/user';
+import { Sesion } from '../../ngrx/auth/auth.model';
 
 @Component({
   selector: 'app-home',
@@ -16,15 +21,30 @@ import { FooterComponent } from "../../footer/footer.component";
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  courses$!: Observable<Course[]>;
 
-  constructor(private homeApiService: HomeApiService) { }
+  courses$!: Observable<Course[]>;
+  usuario$: Observable<User | null>;
+
+  constructor(
+    private homeApiService: HomeApiService,
+    private modalService: NgbModal,
+    private store: Store<{ sesion: Sesion }>
+  ) {
+    this.usuario$ = this.store.select(state => state.sesion.usuarioLogueado);
+  }
 
   ngOnInit(): void {
     this.courses$ = this.homeApiService.getCursos();
   }
 
-  private loadCourses() {
-    this.courses$ = this.homeApiService.getCursos();
+  abrirModal(courseId: string) {
+    const modalRef = this.modalService.open(ModalAddFormInformationComponent, { size: 'lg' });
+    modalRef.componentInstance.courseId = courseId;
+
+    modalRef.closed.subscribe((data) => {
+      if (data) {
+        console.log("Información enviada:", data);
+      }
+    });
   }
 }
